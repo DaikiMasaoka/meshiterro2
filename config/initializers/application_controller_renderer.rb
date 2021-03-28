907fd69cad8a2ba1c24db9ec02409e9f815f6a66
DaikiMasaoka <masadai.29@icloud.com>
2021-03-28 09:37:37 +0000

    5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '58966d1265de5f271e03dba65f8eafc2a0b29134abd3d2ded1ec0a0c81fc8bcdce221ce565ef15d06cc9cd2ef3afadd83f98f04b85c451c7d1fce4ef3004bfe0'
